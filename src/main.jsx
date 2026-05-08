2fd53a51033b5d3458704f9fb71c55d898a8dcc6
albatrosh-jpeg <75555113+albatrosh-jpeg@users.noreply.github.com>
2026-05-08 16:02:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import ArcoplazaLanding from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ArcoplazaLanding />
   </React.StrictMode>,
 )

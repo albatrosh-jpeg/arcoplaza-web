21cef6a5d08a2598f9846130cb0e7bc47299b61f
Álvaro del Arco <alvarcofer@gmail.com>
2026-05-15 11:00:59 +0200

    Use verified Arcoplaza domain in Resend

--- api/contact.js
+++ api/contact.js
@@ -58,7 +58,7 @@ export default async function handler(req, res) {
     })
 
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'web@arcoplazaasesores.com',
       to: 'aaff@centralenergyasesores.com',
       subject: 'Nueva solicitud web Arcoplaza',
       html: `
